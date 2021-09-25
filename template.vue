305400fb0dbacb8b15eeb94c39e5e8c660040716
guowf <380482989@qq.com>
2021-09-25 16:21:09 +0800

    Create template.vue

---
+++ template.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page">
+        <!-- 主动打招呼弹窗-有依赖于BasePhoneLogin，千万别删除BasePhoneLogin -->
+        <BasePhoneLogin ref="phoneLogin"></BasePhoneLogin>
+        <BasePopSayHello ref="BasePopSayHello"></BasePopSayHello>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    },
+    computed: {},
+    components: {},
+    methods: {},
+    onLoad() {},
+    onReady() {},
+    onShow() {},
+    onHide() {},
+    async onReady(){
+        // 检查是否打开主动打招呼弹窗-
+        await this.$refs?.BasePopSayHello.open()
+    },
+    onUnload() {},
+    onPullDownRefresh() {},
+    onReachBottom() {},
+    onPageScroll() {}
+}
+</script>
+<style lang="scss" scoped>
+</style>
